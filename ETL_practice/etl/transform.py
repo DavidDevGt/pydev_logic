e782870d9ec49b4ef5f444c575d6c13964ed9e73
DavidDevGt <josuedavidvl18@gmail.com>
2024-01-22 03:00:59 -0600

    Refactor ETL and data generation code

--- ETL_practice/etl/transform.py
+++ ETL_practice/etl/transform.py
@@ -1,7 +1,40 @@
+import pandas as pd
+
+
 class Transformer:
     @staticmethod
-    def transform_data(data):
-        transformed_data = []
-        for row in data:
-            transformed_data = ""
-        return transformed_data
+    def transform_data(file_path):
+        # Load
+        df = pd.read_csv(file_path)
+
+        # Format
+        df['Precio'] = df['Precio'].astype(float)
+        df['Total'] = df['Total'].astype(float)
+        df['Cantidad'] = df['Cantidad'].astype(int)
+
+        # Analyze popular products and brands
+        popularidad = df.groupby(
+            ['Marca', 'Producto']).size().reset_index(name='Ventas')
+
+        # Range of prices and average
+        precios = df.groupby('Producto').agg(
+            {'Precio': ['min', 'max', 'mean']}).reset_index()
+        precios.columns = ['Producto', 'Precio_Min',
+                           'Precio_Max', 'Precio_Promedio']
+
+        # Predictions stock
+        stock = df.groupby('Producto').agg({'Cantidad': 'sum'}).reset_index()
+        stock.columns = ['Producto', 'Cantidad_Total_Vendida']
+
+        return popularidad, precios, stock
+
+FILE_PATH = "data/raw_data/sales_data.csv"
+
+if __name__ == "__main__":
+    popularidad, precios, stock = Transformer.transform_data(FILE_PATH)
+    print("Popularidad por Marca y Producto:")
+    print(popularidad)
+    print("\nRango de Precios y Promedios:")
+    print(precios)
+    print("\nPredicción de Necesidades de Stock:")
+    print(stock)
--- ETL_practice/utils/data_generator.py
+++ ETL_practice/utils/data_generator.py
@@ -1,22 +1,36 @@
-import csv
+import pandas as pd
+from faker import Faker
 import random
+import numpy as np
+import datetime
 
+fake = Faker()
 
 def generate_fake_data(file_path, num_records):
-    products = ["Laptop", "Tablet", "Smartphone", "Desktop"]
-    brands = ["Dell", "Apple", "HP", "Samsung", "Lenovo", "Asus", "Acer"]
+    start_date = datetime.date(2023, 1, 1)
+    end_date = datetime.date(2023, 12, 31)
 
-    with open(file_path, 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(["ID", "Producto", "Marca", "Precio", "Cantidad"])
+    ids = range(1, num_records + 1)
+    fechas = [fake.date_between(start_date=start_date, end_date=end_date) for _ in range(num_records)]
+    clientes = [fake.name() for _ in range(num_records)]
+    productos = np.random.choice(["Laptop", "Desktop", "Monitor"], size=num_records)
+    marcas = np.random.choice(["Dell", "Apple", "HP", "Samsung", "Lenovo", "Asus", "Acer"], size=num_records)
+    precios = np.random.uniform(3800, 8050, size=num_records)
+    cantidades = np.random.randint(1, 4, size=num_records)
+    totales = precios * cantidades
 
-        for i in range(num_records):
-            product = random.choice(products)
-            brand = random.choice(brands)
-            price = round(random.uniform(3800, 8050), 2)
-            quantity = random.randint(1, 4)
-            writer.writerow([i + 1, product, brand, price, quantity])
+    df = pd.DataFrame({
+        "ID Venta": ids,
+        "Fecha": fechas,
+        "Cliente": clientes,
+        "Producto": productos,
+        "Marca": marcas,
+        "Precio": precios,
+        "Cantidad": cantidades,
+        "Total": totales
+    })
 
+    df.to_csv(file_path, index=False)
 
 if __name__ == "__main__":
-    generate_fake_data('tech_products.csv', 500)
+    generate_fake_data("sales_data.csv", 100000)
